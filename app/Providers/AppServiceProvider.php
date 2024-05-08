e6027fce981a6e0257155757102ab23c9a1dab38
Daffa Yuslim <andidaffa75@gmail.com>
2024-05-08 20:58:20 +0800

    feat: language switcher

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->visible(outsidePanels: true)
+                ->flags([
+                    'en' => asset('assets/images/us.svg'),
+                    'id' => asset('assets/images/id.svg'),
+                ])
+                ->locales(['en', 'id']); // also accepts a closure
+        });
     }
 }

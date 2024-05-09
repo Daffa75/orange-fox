9dd0ab323b779cc87600e00b7d03ff8679832aa6
Daffa Yuslim <andidaffa75@gmail.com>
2024-05-09 21:19:52 +0800

    feat: inbox role

--- app/Filament/Resources/InboxResource.php
+++ app/Filament/Resources/InboxResource.php
@@ -9,6 +9,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -18,6 +19,18 @@ class InboxResource extends Resource
     protected static ?string $model = Inbox::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getEloquentQuery(): Builder
+    {
+        if (auth()->user()->hasRole('Admin Perikanan')) {
+            return parent::getEloquentQuery()->where('source', 'Perikanan');
+        } else if (auth()->user()->hasRole('Admin Peternakan')) {
+            return parent::getEloquentQuery()->where('source', 'Peternakan');
+        } else {
+            return parent::getEloquentQuery();
+        }
+    }
+
 
     public static function form(Form $form): Form
     {
@@ -33,6 +46,10 @@ class InboxResource extends Resource
                 Forms\Components\Textarea::make('message')
                     ->required()
                     ->columnSpanFull(),
+                Forms\Components\TextInput::make('source')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpanFull(),
             ]);
     }
 
@@ -41,27 +58,40 @@ class InboxResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
+                    ->sortable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
+                    ->sortable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('message')
+                    ->sortable()
+                    ->wrap()
+                    ->lineClamp(2)
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('source')
+                    ->sortable()
                     ->wrap()
                     ->lineClamp(2)
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->toggleable(isToggledHiddenByDefault: false),
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                SelectFilter::make('source')
+                    ->options([
+                        'Inskoperasi' => 'Inskoperasi',
+                        'Perikanan' => 'Perikanan',
+                        'Perternakan' => 'Perternakan',
+                    ])
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
